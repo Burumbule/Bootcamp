5ec77826a2cd0b397076fb5c7aebfb6f38d802d8
Burumbule <127529070+Burumbule@users.noreply.github.com>
2023-03-17 18:21:25 +0300

    Add files via upload

---
+++ bootcampgame/js/Gamemenu.js
@@ -0,0 +1,26 @@
+export const createGameMenu=()=>{
+	const title = document.createElement("h2");
+	title.classList.add(".game-menu__title");
+	const gameSection = document.querySelector(".game-section__conteiner");
+	gameSection.innerHTML="";
+
+	title.textContent="СЛОЖНОСТЬ ИГРЫ";
+
+	const createMenuButton = (difficulty)=>{
+		const button = document.createElement("button");
+		button.classList.add(".game-menu__btn");
+		if (difficulty==1) difficulty="лёгкий";
+		else if (difficulty==2) difficulty="средний";
+		else if (difficulty==3) difficulty="сложный";
+		button.textContent = difficulty;
+		button.addEventListener("click", ()=>{})
+		return button;
+	}
+
+	gameSection.append(
+		title,
+		createMenuButton(1),
+		createMenuButton(2),
+		createMenuButton(3)
+	);
+}
